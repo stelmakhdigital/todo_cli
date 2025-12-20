d62dada180665be6bd764db27b2eb0e65e0289da
Di <budaev@MacBook-Air-Di.local>
2025-12-21 00:32:41 +0500

    удалил временные код из main - теперь работа с приложением реализована через CLI cobra

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"todo_cli/internal/manager"
-	"todo_cli/internal/storage"
-)
+import "todo_cli/cmd"
 
 func main() {
-	store := &storage.FileStorage{}
-	filter := &manager.FilterTasks{}
-	mgr := manager.NewManager(store, filter)
-
-	// allTasks, err := mgr.GetAll()
-	// if err != nil {
-	// 	log.Error(err)
-	// }
-	// mgr.Render(allTasks)
-
-	// testTask := map[string]string{
-	// 	"Title":       "Test task",
-	// 	"Description": "bla vla bla",
-	// }
-
-	// mgr.Create(testTask)
-
-	// mgr.Start(8)
-
-	// allTasks2, err2 := mgr.GetAll()
-	// if err2 != nil {
-	// 	log.Error(err2)
-	// }
-	mgr.Show(10)
-
+	cmd.Execute()
 }
